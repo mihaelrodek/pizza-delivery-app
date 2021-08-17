20a1d4f351cbce2929fe445c065a631455002d3d
mihaelrodek <mihael.rodek1@gmail.com>
2021-08-17 16:41:23 +0200

    Minor fixes

--- src/main/java/com/agency04/sbss/pizza/PizzaDeliveryService.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaDeliveryService.java
@@ -3,8 +3,9 @@ package com.agency04.sbss.pizza;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class PizzaDeliveryService {
 
     private PizzeriaService pizzeriaService;

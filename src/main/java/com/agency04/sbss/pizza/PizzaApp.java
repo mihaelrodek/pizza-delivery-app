2b54731862b9f18bea82c3557e507e5c74799af9
mihaelrodek <mihael.rodek1@gmail.com>
2021-08-17 16:50:08 +0200

    Merge master into week-2

--- src/main/java/com/agency04/sbss/pizza/PizzaApp.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaApp.java
@@ -1,8 +1,10 @@
 package com.agency04.sbss.pizza;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@SpringBootApplication
 public class PizzaApp {
 
     public static void main(String[] args) {

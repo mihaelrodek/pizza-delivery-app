48371d9b357dbe1a020c65b81665ffa9a2763283
mihaelrodek <mihael.rodek1@gmail.com>
2021-08-17 18:31:52 +0200

    Merged master into week3

--- src/main/java/com/agency04/sbss/pizza/PizzaConfig.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaConfig.java
@@ -1,5 +1,8 @@
 package com.agency04.sbss.pizza;
 
+import com.agency04.sbss.pizza.services.PizzeriaService;
+import com.agency04.sbss.pizza.services.impl.DomenicoPizzeria;
+import com.agency04.sbss.pizza.services.impl.RajPizzeria;
 import org.springframework.context.annotation.*;
 
 @Configuration
